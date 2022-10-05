4fda74a56741cbbea2d7b2ec9bf16e00eb0ee59b
guribeir <guribeir@student.42.rio>
2022-10-05 16:28:30 +0200

    feat: add echo built-in

--- sources/echo.c
+++ sources/echo.c
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 16:19:44 by guribeir          #+#    #+#             */
-/*   Updated: 2022/10/05 16:38:06 by guribeir         ###   ########.fr       */
+/*   Created: 2022/10/05 16:20:44 by guribeir          #+#    #+#             */
+/*   Updated: 2022/10/05 16:37:06 by guribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+void	echo(char **str)
 {
-	char	**strs;
+	int	i;
+	int	flag;
 
-	strs = ft_split("-n Só bora -n lá então", ' ');
-	echo(strs);
-	return (0);
+	i = 0;
+	flag = 0;
+	if (ft_strncmp(str[i], "-n", 2) == 0)
+	{
+		i++;
+		flag = -1;
+	}
+	while (str[i])
+	{
+		ft_putstr_fd(str[i], 1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
+	if (flag == 0)
+		ft_putchar_fd('\n', 1);
 }
