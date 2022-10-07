f81c922529d7796d99a1f9157230e8760a73a29b
guribeir <guribeir@student.42.rio>
2022-10-07 15:01:58 +0200

    fix: fix ^C on prompt

--- sources/signal.c
+++ sources/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:57:34 by coder             #+#    #+#             */
-/*   Updated: 2022/10/07 16:59:50 by coder            ###   ########.fr       */
+/*   Updated: 2022/10/07 14:45:50 by guribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	sig_handler(int	sig)
 	if (sig == SIGINT)
 	{
 		ft_putchar_fd('\n', 1);
-		rl_reset_line_state();
+		rl_on_new_line();
+		rl_replace_line("", 1);
 		rl_redisplay();
 	}
 }
